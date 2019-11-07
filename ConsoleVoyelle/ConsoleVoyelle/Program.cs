5b5dee81a0e2971dbcb0bb6fa79f8a281edc21b4
antoine hagiyannakis <antoine.hagiyannakis@student.hepl.be>
2019-11-07 16:48:05 +0100

    Super projet !

---
+++ ConsoleVoyelle/ConsoleVoyelle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleVoyelle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Antoine à un beau chapeau :D");
+            Console.ReadKey();
+        }
+    }
+}
